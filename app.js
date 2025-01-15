abbd2292c33728b4a00cb269f067654db2290b16
Heather Toal <htoal@balanceinnovations.com>
2025-01-15 14:32:11 -0600

    Added logout button and error message

--- app.js
+++ app.js
@@ -1,19 +1,77 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
 app.set('view engine', 'pug');
 
+
+
+app.use((req, res, next) => {
+    console.log("Hello");
+    const err = new Error('Oh no!');
+    next(err);
+});
+
+app.use((req, res, next) => {
+    console.log("world!");
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', {name});
+    } else {
+        res.redirect('/hello');
+    }
+    
 });
 
-app.get('/home', (req, res) => {
-    res.send('<h1>Hello, this is the home page.</h1!>');
+app.get('/cards', (req, res) => {
+    res.render('card', {prompt: "Who is buried in grant's tomb?", hint: "Think about whose tomb it is."} );
 });
 
+app.get('/sandbox', (req, res) => {
+    res.render('sandbox')
+});
+
+app.get('/hello', (req, res) => {
+    const name = req.cookies.username;
+    if(name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+});
+
+app.post('/hello', (req, res) => {
+        res.cookie('username', req.body.username);
+        res.redirect('/');
+});
+
+app.get('/goodbye', (req, res) => {
+    res.send('Goodbye! Redirecting you to the hello form.')
+})
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username', {path: '/'});
+    res.redirect('/hello');
+})
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status
+    res.render('error');
+})
 
 app.listen(3000, () => {
 console.log('the application is running on localhost:3000')
 
 });
+
+//create two columns and put in first and last names
